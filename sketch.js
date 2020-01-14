d9e77e52fbfeb561bfe00e183b0f151a84059fd0
Agnese Bartolucci <agnesebartolucci505@gmail.com>
2020-01-14 15:38:59 +0100

    Re-evaluation

--- sketch.js
+++ sketch.js
@@ -1,25 +1,37 @@
-function preload(){
-}
-
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   angleMode(DEGREES);
 }
 
 function draw() {
-  background(60, 60, 60);
+  background(50);
 
   var posX = mouseX - height/1.2;
   var posY = mouseY - width/1.2;
 
   pointLight(255, 85, 255, posX, posY, 0);
   pointLight(0, 255, 55, -posX, posY, 0);
-  pointLight(45, 100, 255, posX, -posY, 0);
-
-  camera(posX, posY, 50, -50, 0, 0, 0, 1, 0);
+  pointLight(255, 70, 0, posX, -posY, 0);
 
   noStroke();
   ambientMaterial(255);
-  rotateX(posX);
-  torus(150, 50);
+  rotateY(frameCount);
+  sphere(100);
+
+  noStroke();
+  specularMaterial(100);
+  rotateX(50);
+  torus(150, 15);
+
+  noStroke();
+  ambientMaterial(255);
+  rotateZ(frameCount);
+  translate(400, 0);
+  sphere(30);
+
+  noStroke();
+  specularMaterial(100);
+  rotateY(frameCount);
+  translate(80, 0);
+  sphere(15);
 }
